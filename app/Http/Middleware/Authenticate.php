1891c7b8e8a3bdd731f1d5da721e0d5743b932e2
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-03-01 12:40:34 +0900

    middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,10 +15,8 @@ class Authenticate
     protected $auth;
 
     /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
+     * Authenticate constructor.
+     * @param Auth $auth
      */
     public function __construct(Auth $auth)
     {
@@ -28,16 +26,16 @@ class Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
-        }
+//        if ($this->auth->guard($guard)->guest()) {
+//            return response('Unauthorized.', 401);
+//        }
 
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-
 $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->group([], function () use ($app) {
+$app->group(['middleware' => 'auth'], function () use ($app) {
+    #region users
     $app->get('/users', ['as' => 'users', 'uses' => 'UserController@indexAction']);
     $app->get('/users/new', ['as' => 'users_new', 'uses' => 'UserController@newAction']);
-    $app->get('/users/{id}', ['as' => 'users_show', 'uses' => 'UserController@showAction']);
-    $app->post('/users/{id}', ['as' => 'users_create', 'uses' => 'UserController@createAction']);
-    $app->get('/users/{id}/edit', ['as' => 'users_edit', 'uses' => 'UserController@editAction']);
-    $app->put('/users/{id}', ['as' => 'users_update', 'uses' => 'UserController@updateAction']);
-    $app->delete('/users/{id}', ['as' => 'users_delete', 'uses' => 'UserController@deleteAction']);
+    $app->get('/users/{id:[1-9]+}', ['as' => 'users_show', 'uses' => 'UserController@showAction']);
+    $app->post('/users/{id:[1-9]+}', ['as' => 'users_create', 'uses' => 'UserController@createAction']);
+    $app->get('/users/{id:[1-9]+}/edit', ['as' => 'users_edit', 'uses' => 'UserController@editAction']);
+    $app->put('/users/{id:[1-9]+}', ['as' => 'users_update', 'uses' => 'UserController@updateAction']);
+    $app->delete('/users/{id:[1-9]+}', ['as' => 'users_delete', 'uses' => 'UserController@deleteAction']);
+    #endregion
+
 });
