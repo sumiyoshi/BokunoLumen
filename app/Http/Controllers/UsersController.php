84a45e33dc70e829bfa0265d67766c30e4e79d28
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-03-01 17:09:51 +0900

    ざっくりコントローラー

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,41 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UsersService;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function __construct()
-    {
+    /**
+     * @var UsersService
+     */
+    private $service;
 
+    public function __construct(UsersService $service)
+    {
+        $this->service = $service;
     }
 
     public function indexAction()
     {
-        return view('users.index', ['id' => 1, 'name' => 'test']);
+        $list = $this->service->getList();
+        return view('users.index', compact('list'));
     }
 
     public function showAction($id)
     {
+        $user = $this->service->get($id);
+        return view('users.show', compact('user'));
     }
 
-    public function newAction(Request $request)
+    public function newAction()
     {
+        $user = $this->service->createEntity();
+        return view('users.new', compact('user'));
     }
 
     public function createAction(Request $request)
     {
+        if ($this->service->save($request->all())) {
+
+        } else {
+
+        }
     }
 
     public function editAction($id)
     {
+        $user = $this->service->get($id);
+        return view('users.edit', compact('user'));
     }
 
     public function updateAction(Request $request, $id)
     {
+        if ($this->service->save($request->all(), $id)) {
+
+        } else {
+
+        }
     }
 
     public function deleteAction($id)
     {
+        $this->service->delete($id);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,6 +6,5 @@
 
 
 @section('content')
-    <h1>Hi {{ $name }} !!</h1>
-    <h2>ID is {{ $id }} .</h2>
+
 @endsection
