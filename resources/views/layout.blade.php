ca3ad4cf1e7db59cca83ae250c21de51919064bf
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-03-03 11:27:37 +0900

    微調整

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
     <div class="col-md-3 left_col">
         <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-                <a href="" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+                <a href="{{route('home')}}" class="site_title"><i class="fa fa-crosshairs"></i> <span>Gentelella Alela!</span></a>
             </div>
 
             <div class="clearfix"></div>
