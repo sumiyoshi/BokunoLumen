1f9b7f9f10bfff6663b11839b43143f5cb64a5c2
sumimaru <sumiyoshi102@gmail.com>
2017-03-09 14:09:40 +0900

    Create error.blade.php

---
+++ resources/views/error.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ERROR</title>
+
+    <link rel="stylesheet" href="/node_modules/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/node_modules/gentelella/vendors/font-awesome/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="/node_modules/gentelella/vendors/animate.css/animate.min.css"/>
+    <link rel="stylesheet" href="/node_modules/gentelella/build/css/custom.min.css"/>
+</head>
+
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        <!-- page content -->
+        <div class="col-md-12">
+            <div class="col-middle">
+                <div class="text-center text-center">
+                    <h1 class="error-number">404</h1>
+                </div>
+            </div>
+        </div>
+        <!-- /page content -->
+    </div>
+</div>
+</body>
+</html>
