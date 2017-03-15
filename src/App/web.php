05dcf151b081eab06295697d16318687f1f67d7d
sumimaru <sumiyoshi102@gmail.com>
2017-03-15 10:50:44 +0900

    Update web.php

--- src/App/web.php
+++ src/App/web.php
@@ -7,15 +7,7 @@ $app->group(['middleware' => ['auth']], function () use ($app) {
     #endregion
 
     $app->group(['middleware' => ['access']], function () use ($app) {
-        #region users
-        $app->get('/users', ['as' => 'users', 'uses' => 'UsersController@indexAction']);
-        $app->get('/users/new', ['as' => 'users_new', 'uses' => 'UsersController@newAction']);
-        $app->get('/users/{id:[0-9]+}', ['as' => 'users_show', 'uses' => 'UsersController@showAction']);
-        $app->post('/users/', ['as' => 'users_create', 'uses' => 'UsersController@createAction']);
-        $app->get('/users/{id:[0-9]+}/edit', ['as' => 'users_edit', 'uses' => 'UsersController@editAction']);
-        $app->post('/users/{id:[0-9]+}/update', ['as' => 'users_update', 'uses' => 'UsersController@updateAction']);
-        $app->post('/users/{id:[0-9]+}/delete', ['as' => 'users_delete', 'uses' => 'UsersController@deleteAction']);
-        #endregion
+        resources($app, 'users');
     });
 
 });
